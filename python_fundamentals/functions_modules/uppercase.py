f2c1b66e4cd87f60bedd1ae29a6566ac75893681
maxence30 <henous.maxence@outlook.fr>
2026-05-10 23:57:34 +0200

    f

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 def uppercase(str):
     """
     Affiche une chaîne en majuscules suivie d'un saut de ligne.
@@ -6,11 +6,8 @@ def uppercase(str):
     result = ""
 
     for c in str:
-        ascii_val = ord(c)
-
-        # Si c'est une lettre minuscule (a-z)
-        if 97 <= ascii_val <= 122:
-            result += chr(ascii_val - 32)
+        if ord('a') <= ord(c) <= ord('z'):
+            result += chr(ord(c) - 32)
         else:
             result += c
 
