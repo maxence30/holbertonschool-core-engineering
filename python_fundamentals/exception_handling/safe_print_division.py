a13b4ce91ad999a74fcef0b68c5c42fed79bed51
maxence30 <henous.maxence@outlook.fr>
2026-05-11 11:57:07 +0200

    ok

--- python_fundamentals/exception_handling/safe_print_division.py
+++ python_fundamentals/exception_handling/safe_print_division.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a / b
+    except (ZeroDivisionError, TyperError):
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
