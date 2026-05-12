32f1b329ca4fdc52c1961bad1a7ad7e8420348d0
maxence30 <henous.maxence@outlook.fr>
2026-05-12 11:18:35 +0200

    task 2

--- python_oop/classes_and_object_model/1-rectangle.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""ajout dun carré"""
-
-class Square:
-    def __init__(self, size):
-        self.__ = size
-        
--- python_oop/classes_and_object_model/1-square.py
+++ python_oop/classes_and_object_model/1-square.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""ajout dun carré"""
+
+class Square:
+    def __init__(self, size):
+        self.__ = size
+        
