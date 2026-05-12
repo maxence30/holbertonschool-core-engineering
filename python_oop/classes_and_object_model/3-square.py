83ef3cd55c1f6accc9ceafdb7d0b691aee6213ad
maxence30 <henous.maxence@outlook.fr>
2026-05-12 14:12:30 +0200

    task 4

--- python_oop/classes_and_object_model/3-square.py
+++ python_oop/classes_and_object_model/3-square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""Square module."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
+    
