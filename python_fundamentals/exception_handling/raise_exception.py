3029cded7a4901fe7f24acbd71a49fa2e0b99e2f
maxence30 <henous.maxence@outlook.fr>
2026-05-12 09:34:29 +0200

    project 1

--- python_fundamentals/exception_handling/raise_exception.py
+++ python_fundamentals/exception_handling/raise_exception.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""Module that raises a TypeError exception."""
+
+
+def raise_exception():
+    """Raise a TypeError exception."""
+    raise TypeError
--- python_fundamentals/exception_handling/raise_exception_msg.py
+++ python_fundamentals/exception_handling/raise_exception_msg.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""Module that raises a NameError exception with a message."""
+
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a custom message."""
+    raise NameError(message)
