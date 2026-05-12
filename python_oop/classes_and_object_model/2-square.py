d8cc3d0b367d481aea69ca59259fb69f4e81f18c
maxence30 <henous.maxence@outlook.fr>
2026-05-12 14:11:05 +0200

    task 3

--- python_oop/classes_and_object_model/2-square.py
+++ python_oop/classes_and_object_model/2-square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""Square module."""
+
+
+class Square:
+    """Class creation."""
+
+    def __init__(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
