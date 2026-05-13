4e7278e103dc7a7c82b6fdc67f5c43d2adadc718
maxence30 <henous.maxence@outlook.fr>
2026-05-13 16:00:58 +0200

    w

---
+++ python_fundamentals/core_data_structures/common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module defines a function to find common elements in two sets."""
+
+
+def common_elements(set_1, set_2):
+    """Return a set of elements present in both sets."""
+    return set_1 & set_2
