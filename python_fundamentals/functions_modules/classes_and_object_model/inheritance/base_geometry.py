7bc990fe5a4920e1c162cf371f8e367b54241f8b
maxence30 <henous.maxence@outlook.fr>
2026-05-13 15:35:10 +0200

    ok

--- python_fundamentals/functions_modules/classes_and_object_model/inheritance/base_geometry.py
+++ python_fundamentals/functions_modules/classes_and_object_model/inheritance/base_geometry.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
+"""Module defines a BaseGeometry class."""
+
 
 class BaseGeometry:
+    """Base class for geometry."""
+
     def area(self):
+        """Raise an exception because area is not implemented."""
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
+        """Validate that value is a positive integer."""
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
-        
+
         if value <= 0:
-            raise ValueError(f"{name}must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
+        
