00059c101964af493626c1ea1a6d3cd1f912bd88
maxence30 <henous.maxence@outlook.fr>
2026-05-13 16:00:07 +0200

    x

---
+++ python_fundamentals/core_data_structures/add_tuple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module defines a function that adds two tuples."""
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    """Return a tuple with 2 integers, adding values from two tuples."""
+    a1 = tuple_a[0] if len(tuple_a) > 0 else 0
+    a2 = tuple_a[1] if len(tuple_a) > 1 else 0
+
+    b1 = tuple_b[0] if len(tuple_b) > 0 else 0
+    b2 = tuple_b[1] if len(tuple_b) > 1 else 0
+
+    return (a1 + b1, a2 + b2)
