44df67f9976143bb644945c1cea4f3843309838f
maxence30 <henous.maxence@outlook.fr>
2026-05-13 15:59:30 +0200

    v

---
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module defines a function to print a matrix of integers."""
+
+
+def print_matrix_integer(matrix=[[]]):
+    """Print a matrix of integers with proper formatting."""
+    for row in matrix:
+        print(" ".join("{:d}".format(i) for i in row))
